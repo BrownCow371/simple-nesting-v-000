48bd93a6f7e8f776e97cc0452829e80e34bcf513
Jamie Brown <jmbrowncow@gmail.com>
2018-07-03 15:42:48 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -55,8 +55,8 @@ def dennis_ritchies_language
           :known_for => "Unix",
           :languages => ["C"]
         }
+	     }
 	programmer_hash[dennis_ritchie][languages][0]
-     }
 end
 
 def adding_matz
@@ -83,6 +83,7 @@ def adding_matz
      }
 
 programmer_hash[:yukihiro_matsumoto]={:known_for => "Ruby",:languages => ["LISP", "C"] }
+programmer_hash
 end
 
 def changing_alan
@@ -104,7 +105,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 		 programmer_hash[:alan_kay][:known_for]=alans_new_info
-
+		 programmer_hash
 
 end
 
@@ -124,5 +125,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+ programmer_hash[:dennis_ritchie][:languages]<<"Assembly"
+ programmer_hash
 end
